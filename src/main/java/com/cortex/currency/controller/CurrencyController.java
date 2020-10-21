532db38092c50d0b59258cdfc446f07c8eb45654
Venicios Ribeiro <mvrdutra@gmail.com>
2020-10-21 15:55:30 -0300

    Adiciona documentação Swagger no CurrencyController

--- src/main/java/com/cortex/currency/controller/CurrencyController.java
+++ src/main/java/com/cortex/currency/controller/CurrencyController.java
@@ -5,6 +5,8 @@ import com.cortex.currency.dto.RequestDTO;
 import com.cortex.currency.dto.ResponseDTO;
 import com.cortex.currency.enums.Moeda;
 import com.cortex.currency.service.ProducerService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -25,10 +27,16 @@ public class CurrencyController {
     @Autowired
     private ProducerService producerService;
 
+    @ApiOperation(value = "Retorna o valor da conversão da Moeda Origem para a Moeda Destino")
     @GetMapping(value = {getPathConverter, getConverterWithPriority}, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody
-    ResponseEntity<ResponseDTO> converte(@PathVariable Moeda moedaOrigem, @PathVariable Moeda moedaDestino, @PathVariable Double valorDesejado,
-                                         @PathVariable LocalDate dataCotacao, @PathVariable(required = false) boolean priority) {
+    ResponseEntity<ResponseDTO> converte(
+            @ApiParam(value = "Moeda que terá seu valor convertido para a Moeda Destino. Ex.: GBP", example = "GBP", defaultValue = "GBP") @PathVariable Moeda moedaOrigem,
+            @ApiParam(value = "Moeda que apresentará o valor final convertido da Moeda Origem. Ex.: BRL", example = "BRL", defaultValue = "BRL") @PathVariable Moeda moedaDestino,
+            @ApiParam(value = "Valor que será usado para conversão na Moeda Destino. Ex.: 1.5", example = "1.5") @PathVariable Double valorDesejado,
+            @ApiParam(value = "Data da Cotação no Banco Central. Ex.: dd-MM-yyyy", example = "21-10-2020") @PathVariable LocalDate dataCotacao,
+            @ApiParam(value = "Indica se tem prioridade de conversão sobre os outros pedidos (não está em uso)", example = "false", defaultValue = "false", readOnly = true, allowableValues = "false") @PathVariable(required = false) boolean priority
+    ) {
 
         CurrencyDTO currency = CurrencyDTO
                 .builder()
